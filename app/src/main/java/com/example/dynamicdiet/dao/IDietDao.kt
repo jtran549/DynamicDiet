b1e02286b15e788c24033770de9cc6a2f0ddc502
tracyjn@mail.uc.edu <tracyjn@mail.uc.edu>
2022-03-06 16:40:32 -0500

    Created Diet dto, dao, service

---
+++ app/src/main/java/com/example/dynamicdiet/dao/IDietDao.kt
@@ -0,0 +1,5 @@
+interface IDietDao {
+    fun getDietInfo() {
+
+    }
+}
---
+++ app/src/main/java/com/example/dynamicdiet/service/DietService.kt
@@ -0,0 +1,17 @@
+class DietService {
+    suspend fun getDietInfo(): Diet {
+        return Diet('Keto Diet', 2000);
+    }
+
+    suspend fun createDiet(){
+
+    }
+
+    suspend fun updateDiet(){
+
+    }
+
+    suspend fun deletePerson(){
+
+    }
+}
