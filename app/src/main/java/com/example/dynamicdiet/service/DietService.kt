198583b0b17866199b15659e2791f6416cd70913
tracyjn@mail.uc.edu <tracyjn@mail.uc.edu>
2022-03-06 17:40:39 -0500

    Added documentation to DietService

--- app/src/main/java/com/example/dynamicdiet/service/DietService.kt
+++ app/src/main/java/com/example/dynamicdiet/service/DietService.kt
@@ -1,17 +1,45 @@
 class DietService {
-    suspend fun getDietInfo(): Diet {
+    /**
+     * Fetches diet information for the user
+     *
+     * @param userId id of the user
+     * @return Diet object
+     */
+    suspend fun getDietInfo(var userId: Int): Diet {
         return Diet('Keto Diet', 2000);
     }
 
-    suspend fun createDiet(){
+    /**
+     * Creates a new Diet object and inserts it into the database
+     *
+     * @param userId id of the user
+     * @param name name of the diet
+     * @param caloriesPerDay The number of calories per day set for the diet
+     * @return newly created Diet object
+     */
+    suspend fun createDiet(var userId: Int, var name: String, var caloriesPerDay: Int): Diet {
 
     }
 
-    suspend fun updateDiet(){
+    /**
+     * Updates an already existing Diet record in the database
+     *
+     * @param userId id of the user
+     * @param name name of the diet
+     * @param caloriesPerDay The number of calories per day set for the diet
+     * @return newly updated Diet object
+     */
+    suspend fun updateDiet(var userId: Int, var name: String, var caloriesPerDay: Int): Diet {
 
     }
 
-    suspend fun deleteDiet(){
+    /**
+     * Deletes an existing Diet record from the database
+     *
+     * @param userId id of the user
+     * @param dietId id of the diet record
+     */
+    suspend fun deleteDiet() {
 
     }
 }
