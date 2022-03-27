e1d7dfa494cb4e4d0fa34842fdd0a6ed847eb21a
Herier44 <djherier@yahoo.com>
2022-03-27 16:37:36 -0400

    adding to user

--- app/src/main/java/com/example/dynamicdiet/dao/IUserDAO.kt
+++ app/src/main/java/com/example/dynamicdiet/dao/IUserDAO.kt
@@ -1,4 +1,13 @@
 package com.example.dynamicdiet.dao
 
-class IUserDAO {
+import com.example.dynamicdiet.dto.User
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+abstract class IUserDAO {
+
+    @GET("/getfrom/firebase?")
+    abstract suspend fun getAllUsers(): ArrayList<User>
+
 }
