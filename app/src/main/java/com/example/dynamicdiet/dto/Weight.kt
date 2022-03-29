5a94bb421afb88b79a58fbfe7b9cb6fe46b7b7e5
Yanling <zentgryg@mail.uc.edu>
2022-03-28 23:54:17 -0400

    change class to data class, override toString function and return name and weight.

--- app/src/main/java/com/example/dynamicdiet/dto/Weight.kt
+++ app/src/main/java/com/example/dynamicdiet/dto/Weight.kt
@@ -2,5 +2,8 @@ package com.example.dynamicdiet.dto
 
 import java.util.*
 
-class Weight (var weight: Double, var date: String){
+data class Weight (var weight: Double, var date: String, var name: String){
+    override fun toString(): String {
+        return "$name $weight"
+    }
 }
