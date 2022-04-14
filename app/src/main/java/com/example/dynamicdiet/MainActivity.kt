34f3cd788848e2e973542bca19cf60c43991f3f8
Tran, Jimmy (tranjy) <tranjy@mail.uc.edu>
2022-04-14 16:28:12 -0400

    Changed weight to entry
    The reason for this is I decided calories and weight for the day should be a single object, and when I get the list out from the database it'll be easier to work with

--- app/src/main/java/com/example/dynamicdiet/MainActivity.kt
+++ app/src/main/java/com/example/dynamicdiet/MainActivity.kt
@@ -1,39 +1,23 @@
 package com.example.dynamicdiet
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Call
-import androidx.compose.material.icons.filled.Phone
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
-import androidx.lifecycle.ViewModel
-import com.example.dynamicdiet.dto.Weight
+import com.example.dynamicdiet.dto.Entry
 import com.example.dynamicdiet.ui.theme.DynamicDietTheme
-import com.example.dynamicdiet.ui.theme.Purple500
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
@@ -47,7 +31,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 //            val weightEntries = ArrayList<Weight>()
-            viewModel.weightEntries.add(Weight(weight = 165.0, date = "03-25-2022"))
+            viewModel.weightEntries.add(Entry(weight = 165.0, date = "03-25-2022"))
             DynamicDietTheme {
                 val scaffoldState = rememberScaffoldState()
                 val scope = rememberCoroutineScope()
@@ -59,7 +43,7 @@ class MainActivity : ComponentActivity() {
                                 MainMessage(viewModel = viewModel)
                             }
                             Surface {
-                                MainMenuOptions()
+                                MainMenuOptions(viewModel = viewModel)
                             }
                             Surface(){
                                 DisplayWeightEntries(viewModel = viewModel)
@@ -78,20 +62,20 @@ fun MainMessage(viewModel: MainViewModel) {
             Modifier
                 .fillMaxWidth()
                 .padding(30.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Bottom) {
-            Text("You need to eat ${viewModel.calories} calories today", fontSize = 30.sp)
+            Text("You need to eat 2000 calories today", fontSize = 30.sp)
         }
     }
 }
 
 @Composable
-fun MainMenuOptions(){
+fun MainMenuOptions(viewModel: MainViewModel){
     Row {
         Column (
             Modifier
                 .fillMaxWidth()
                 .padding(30.dp),
             horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-                CallAlertDialog()
+                NewEntryButton(viewModel = viewModel)
                 Button (
                     modifier = Modifier.fillMaxWidth(),
                     onClick = {},
@@ -121,9 +105,9 @@ fun InputWeight(viewModel: MainViewModel, scope:CoroutineScope, scaffoldState: S
                 Button (
                     modifier = Modifier.fillMaxWidth(),
                     onClick = {
-                        var weight = Weight(weight = viewModel.weightInput.toDouble(), date = dateTime).apply {
+                        var weight = Entry(weight = viewModel.weightInput.toDouble(), date = dateTime).apply {
                         }
-                        viewModel.save(weight = weight)
+                        viewModel.save(entry = weight)
                         scope.launch { scaffoldState.snackbarHostState.showSnackbar("Weight: ${viewModel.weightInput} Date: $dateTime") }
                     },
                     content = {Text(text = stringResource(R.string.submit))}
@@ -157,9 +141,9 @@ fun DisplayWeightEntries(viewModel: MainViewModel) {
     }
 }
 @Composable
-fun CallAlertDialog() {
+fun NewEntryButton(viewModel : MainViewModel) {
     val isDialogOpen = remember { mutableStateOf(false)}
-        ShowEntryLogDialogue(isDialogOpen)
+        NewEntryDialogue(isDialogOpen, viewModel)
         Button(
             onClick = {
                 isDialogOpen.value = true
@@ -171,7 +155,9 @@ fun CallAlertDialog() {
         }
 }
 @Composable
-fun ShowEntryLogDialogue(isDialogOpen:MutableState<Boolean>){
+fun NewEntryDialogue(isDialogOpen:MutableState<Boolean>, viewModel: MainViewModel){
+    val simpleDateFormat = SimpleDateFormat("MM-dd-yyy");
+    val dateTime = simpleDateFormat.format(Date())
     if(isDialogOpen.value) {
         Dialog(onDismissRequest = { isDialogOpen.value = false }) {
             Surface(
@@ -199,8 +185,8 @@ fun ShowEntryLogDialogue(isDialogOpen:MutableState<Boolean>){
                     Spacer(modifier = Modifier.padding(10.dp))
 
                     OutlinedTextField(
-                        value = "",//emailVal.value,
-                        onValueChange = { },
+                        value = viewModel.weightInput,
+                        onValueChange = {viewModel.onValueChange(it) },
                         label = { Text(text = "Weight") },
                         placeholder = { Text(text = "Weight") },
                         singleLine = true,
@@ -210,8 +196,8 @@ fun ShowEntryLogDialogue(isDialogOpen:MutableState<Boolean>){
                     Spacer(modifier = Modifier.padding(10.dp))
 
                     OutlinedTextField(
-                        value = "",//passwordVal.value,
-                        onValueChange = { },
+                        value = viewModel.calories,
+                        onValueChange = { viewModel.onValueChange(it)},
                         label = { Text(text = "Calories eaten") },
                         placeholder = { Text(text = "Calories eaten") },
                         singleLine = true,
@@ -223,6 +209,9 @@ fun ShowEntryLogDialogue(isDialogOpen:MutableState<Boolean>){
                     Button(
                         onClick = {
                             isDialogOpen.value = false
+                            var weight = Entry(weight = viewModel.weightInput.toDouble(), date = dateTime).apply {
+                            }
+                            viewModel.save(entry = weight)
                         },
                         modifier = Modifier
                             .fillMaxWidth(0.5f)
--- app/src/main/java/com/example/dynamicdiet/MainViewModel.kt
+++ app/src/main/java/com/example/dynamicdiet/MainViewModel.kt
@@ -4,18 +4,17 @@ import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.dynamicdiet.dto.Weight
+import com.example.dynamicdiet.dto.Entry
 import com.example.dynamicdiet.service.WeightService
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 class MainViewModel(var weightService: WeightService = WeightService()) : ViewModel() {
-    var weights = ArrayList<Weight>()
-    var weightEntries = ArrayList<Weight>()
+    var weights = ArrayList<Entry>()
+    var weightEntries = ArrayList<Entry>()
     var weightInput by mutableStateOf("");
-    var calories = 2000;
+    var calories by mutableStateOf("");
     fun onValueChange (value: String) {
         weightInput = value;
     }
@@ -35,10 +34,10 @@ class MainViewModel(var weightService: WeightService = WeightService()) : ViewMo
                 return@addSnapshotListener
             }
             snapshot?.let {
-                var allWeightEntries = ArrayList<Weight>()
+                var allWeightEntries = ArrayList<Entry>()
                 val documents = snapshot.documents
                 documents.forEach {
-                    val weight = it.toObject(Weight::class.java)
+                    val weight = it.toObject(Entry::class.java)
                     weight?.let {
                         allWeightEntries.add(it)
                     }
@@ -66,9 +65,9 @@ class MainViewModel(var weightService: WeightService = WeightService()) : ViewMo
         val task = document.delete();
     }
 
-    fun save(weight: Weight){
+    fun save(entry: Entry){
         val document = firestore.collection("weightEntries").document()
-        val handle = document.set(weight)
+        val handle = document.set(entry)
         handle.addOnSuccessListener { Log.d("Firebase", "Document saved") }
         handle.addOnFailureListener{Log.e("Firebase", "Save failed $it ")}
     }
