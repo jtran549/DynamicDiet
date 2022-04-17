ab0c4081fd1d228abc0eac9387b6b4fcac0a1d19
BuildTools <herierdd@mail.uc.edu>
2022-04-17 13:20:40 -0400

    updating user to match with firebase and for use in MainActivity

--- app/src/main/java/com/example/dynamicdiet/dto/User.kt
+++ app/src/main/java/com/example/dynamicdiet/dto/User.kt
@@ -4,9 +4,9 @@ import java.util.*
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
-
-@Entity(tableName="user")
-data class User(var userName: String, var weight: List<Weight>, var lastName: String, var firstName: String, var age: String, @PrimaryKey @SerializedName("id") var userId:Int = 0) {
+//var weight: List<Weight>,
+@Entity(tableName="users")
+data class User(var userName: String,  var currentWeight: Int, var lastUpdated: String, var lastName: String, var firstName: String, var age: String, @PrimaryKey @SerializedName("id") var userId:Int = 0) {
     override fun toString(): String {
         return userName
     }
